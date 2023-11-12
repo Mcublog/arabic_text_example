f78dca56cc78511e84bfbb789e4d73b2d5864373
Viacheslav <slava.k@ks2corp.com>
2023-11-12 13:54:58 +0300

    upd examples

--- app/src/main/java/arabic_text_example/App.java
+++ app/src/main/java/arabic_text_example/App.java
@@ -5,19 +5,31 @@
 package arabic_text_example;
 
 import java.nio.charset.Charset;
+import java.text.Normalizer;
 
 public class App {
     public String getGreeting() {
         // Иходная строка в UTF-8
-        String stateOfQuatar = "دولة قطر";
+        String stateOfQuatar = "شامبو ضد القشرة";
         // Реверсим строку, чтобы порядок байт был верный
         String stateOfQuatarRevers = new StringBuilder(stateOfQuatar).reverse().toString();
         byte[] strInBytes = stateOfQuatarRevers.getBytes(Charset.forName("CP1256"));
         for (int j=0; j<strInBytes.length; j++) {
            System.out.format("%02X ", strInBytes[j]);
         }
-        System.out.println("\n");
-        return stateOfQuatar;
+        System.out.println();
+        System.out.println(stateOfQuatarRevers);
+        System.out.println();
+        System.out.println();
+
+        // Удаление огласовок
+        String hello = "مرحبًا"; //بً символ с огласовкой
+        // Печатаем, как есть. Нужен реверс естественно
+        System.err.println(new StringBuilder(hello).reverse().toString());
+        // Печатаем без огласовок https://stackoverflow.com/questions/18580287/how-could-i-remove-arabic-punctuation-form-a-string-in-java
+        hello = Normalizer.normalize(hello, Normalizer.Form.NFD).replaceAll("\\p{M}", "");
+        System.err.println(new StringBuilder(hello).reverse().toString());
+        return "";
     }
 
     public static void main(String[] args) {
--- convertion.py
+++ convertion.py
@@ -2,25 +2,9 @@
 
 import unicodedata as ud
 
-import arabic_reshaper
-from arabic_reshaper import ArabicReshaper
-from arabicscript import ArabStr
-from bidi.algorithm import get_display
-
 HELLO = "مرحبًا"
 QUATAR = "دولة قطر"
 
-# configuration = {
-#     'delete_harakat': True,
-#     'support_ligatures': False,
-#     'language': 'ArabicV2',
-#     'RIAL SIGN': False,
-# }
-# reshaper = ArabicReshaper(configuration=configuration)
-# reshaped_text = arabic_reshaper.reshape(HELLO)
-# print(reshaped_text)
-# for c in reshaped_text:
-#     print(f'{c} U+{ord(c):04X} {ud.name(c)}')
 # s = ud.normalize('NFKD', reshaped_text)
 
 print(QUATAR[::-1])
@@ -31,4 +15,3 @@ for c in QUATAR:
     print(f'{c} U+{ord(c):04X} {ud.name(c)}')
 
 print('-' * 10)
-
